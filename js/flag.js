28140a4f94c1e3e1aa5455a19c1499decd86c4eb
Yumin Park <yumin.park02@gmail.com>
2024-05-18 16:15:14 +0700

    add animated flags

---
+++ js/flag.js
@@ -0,0 +1,51 @@
+
+function animateKrFlag() {
+    var container = document.querySelector('#krFlagContainer')
+    var overlay = document.querySelector('#krOverlay')
+    container.addEventListener('mousemove', function(e){
+        var x = e.offsetX
+        var y = e.offsetY
+        var rotateY = -4/45 * x + 20
+        var rotateX = -2/15 * y + 20
+        overlay.style = `background-position : ${x/5 + y/5}%`
+        container.style = `transform : perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+    })
+    container.addEventListener('mouseout', function(){
+        overlay.style = 'filter : opacity(0)'
+        container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)'
+    })
+}
+
+function animateThFlag() {
+    var container = document.querySelector('#thFlagContainer')
+    var overlay = document.querySelector('#thOverlay')
+    container.addEventListener('mousemove', function(e){
+        var x = e.offsetX
+        var y = e.offsetY
+        var rotateY = -4/45 * x + 20
+        var rotateX = -2/15 * y + 20
+        overlay.style = `background-position : ${x/5 + y/5}%`
+        container.style = `transform : perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+    })
+    container.addEventListener('mouseout', function(){
+        overlay.style = 'filter : opacity(0)'
+        container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)'
+    })
+}
+
+function animateSgFlag() {
+    var container = document.querySelector('#sgFlagContainer')
+    var overlay = document.querySelector('#sgOverlay')
+    container.addEventListener('mousemove', function(e){
+        var x = e.offsetX
+        var y = e.offsetY
+        var rotateY = -4/45 * x + 20
+        var rotateX = -2/15 * y + 20
+        overlay.style = `background-position : ${x/5 + y/5}%`
+        container.style = `transform : perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+    })
+    container.addEventListener('mouseout', function(){
+        overlay.style = 'filter : opacity(0)'
+        container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)'
+    })
+}
